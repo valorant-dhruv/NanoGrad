f57128fb191de37f89bb91d70f868342eb0648b7
valorant-dhruv <78591597+valorant-dhruv@users.noreply.github.com>
2025-07-15 20:29:43 -0700

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -5,9 +5,11 @@ class NeuralNetwork:
 
     if len(layer_sizes) < 2:
             raise ValueError("Neural network must have at least input and output layers")
-    
+
     #We pass in an array which indicates the size of each layer of the neural network
     self.layer_sizes = layer_sizes
+
+    #This contains all the layers in the NN
     self.layers = []
 
     #Creating input layer
@@ -20,25 +22,33 @@ class NeuralNetwork:
       self.layers.append(hidden_layer)
 
     #Creating the output layer
-    output_layer = Layer(layer_sizes[-1], previous_layer=self.layers[-1])
+    output_layer = Layer(layer_sizes[-1], previous_layer=self.layers[-1], output_layer = True)
     self.layers.append(output_layer)
 
-    
+  def get_parameters(self):
+    parameters = []
+    for layer in self.layers:
+      parameters += layer.get_parameters()
+    return parameters
+
   #Now we do a forward pass to find the value of all the neurons in the network
   def forward_pass(self,inputs):
 
     if(len(inputs) != self.layer_sizes[0]):
       raise ValueError("Number of inputs must match the number of neurons in the input layer")
-    
+
     #Finding the values of the neurons for the input layer first
     current_outputs = self.layers[0](inputs)
-        
+
     #Passing through all subsequent layers
     for layer in self.layers[1:]:
         current_outputs = layer()
-        
+
     return current_outputs
-  
+
+  #Now we do backpropogation through the neural network
+
+
   def __repr__(self):
     return f"NeuralNetwork(layers={self.layer_sizes})"
 
@@ -48,4 +58,8 @@ class NeuralNetwork:
   def get_output_value(self):
         """Get the numeric value from the output layer"""
         output_layer = self.layers[-1]
-        return [neuron.neuron_value.value for neuron in output_layer.neurons]
+
+        if len(output_layer.neurons) == 1:
+          return output_layer.neurons[0].neuron_value
+        else:
+          return [neuron.neuron_value for neuron in output_layer.neurons]
